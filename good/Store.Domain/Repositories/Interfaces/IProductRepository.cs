6ca7a8fcc71027978be3d26b8de0662df8724634
sgazevedo <sgazevedo@gmail.com>
2023-10-08 20:30:03 -0300

    feat(): adicionado método getall no IProductRepository

--- good/Store.Domain/Repositories/Interfaces/IProductRepository.cs
+++ good/Store.Domain/Repositories/Interfaces/IProductRepository.cs
@@ -5,5 +5,6 @@ namespace Store.Domain.Repositories.Interfaces
   public interface IProductRepository
   {
     IEnumerable<Product> Get(IEnumerable<Guid> ids);
+    IEnumerable<Product> GetAll();
   }
 }
--- good/Store.Tests/Repositories/FakeProductRepository.cs
+++ good/Store.Tests/Repositories/FakeProductRepository.cs
@@ -1,13 +1,16 @@
 using Store.Domain.Entities;
+using Store.Domain.Queries;
 using Store.Domain.Repositories.Interfaces;
 
 namespace Store.Tests.Repositories
 {
   internal class FakeProductRepository : IProductRepository
   {
-    public IEnumerable<Product> Get(IEnumerable<Guid> ids)
+    private readonly IList<Product> _products;
+
+    public FakeProductRepository()
     {
-      IList<Product> products = new List<Product>
+      _products = new List<Product>
       {
         new Product("Produto 01", 10, true),
         new Product("Produto 02", 10, true),
@@ -15,8 +18,22 @@ namespace Store.Tests.Repositories
         new Product("Produto 04", 10, false),
         new Product("Produto 05", 10, false)
       };
+    }
+
+    public IEnumerable<Product> Get(IEnumerable<Guid> ids)
+    {
+      IList<Product> products = new List<Product>();
+
+      foreach (var id in ids) 
+      { 
+        var product = _products.FirstOrDefault(x => x.Id == id);
+        if (product is not null)
+          products.Add(product);
+      }
 
       return products;
     }
+
+    public IEnumerable<Product> GetAll() => _products.AsQueryable().Where(ProductQueries.GetActiveProducts());
   }
 }
